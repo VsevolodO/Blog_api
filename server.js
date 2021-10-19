b4c5363b5154e33e0634fe52cf635591b2559bd2
CB <cryst1@yandex.ru>
2021-10-19 23:59:15 +0300

    Registration undone

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const app = express()
+const knex = require('knex')({
+    client: 'pg',
+    version: '13',
+    connection: {
+      host : '127.0.0.1',
+      port:'5432',
+      user : 'postgres',
+      password : 'admin',
+      database : 'Blog',
+      timezone: 'UTC',
+      dateStrings: true,
+    }
+});
+
+app.post('/registration', async (req,res) => {
+    console.log(req.query.user)
+    const insert =  await knex('User').insert({user:req.query.user}, {password:req.query.password})
+res.json("status:ok");
+
+
+})
+
 
 app.listen(9000,()=>{
     console.log('server started')

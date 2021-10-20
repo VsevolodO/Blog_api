b52458cb7abfac593427f50a7b9eb5766b380d04
CB <cryst1@yandex.ru>
2021-10-21 01:26:16 +0300

    registration done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const _ = require('underscore')
 const knex = require('knex')({
     client: 'pg',
     version: '13',
@@ -15,11 +16,35 @@ const knex = require('knex')({
 });
 
 app.post('/registration', async (req,res) => {
-    console.log(req.query.user)
-    const insert =  await knex('User').insert({user:req.query.user}, {password:req.query.password})
-res.json("status:ok");
 
+    //console.log(req.query.email != undefined)
+    if (req.query.email != undefined){
 
+        const SameUsername = await knex.select('User').table('User').where('User',req.query.User)
+        const SameEmail = await knex.select('User').table('User').where('email',req.query.email)
+        
+
+        if(_.isEmpty(SameUsername) == false && _.isEmpty(SameEmail) == false){
+            res.status(409).json({message:'Имя пользователя и адресс почты заняты!'})
+        }
+  
+        else if( _.isEmpty(SameUsername) == false){
+            res.status(409).json({message: 'Имя пользователя занято. Пропробуйте другое'})
+        }
+    
+        else if(_.isEmpty(SameEmail) == false) {
+            res.status(409).json({message: 'Адрес электроной почты занят. Пропробуйте другой'})
+        }
+
+        else { 
+            await knex('User').insert({User:req.query.User, Password:req.query.Password, email:req.query.email})
+            .then(res.status(201).json({message: 'ok'}))
+        }
+
+    }
+    else{
+        res.status(409).json({message: 'Не заполнены небходимые поля'})
+    }
 })
 
 

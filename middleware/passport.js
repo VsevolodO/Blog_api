0f3264e8564584a4f8a3a168cd19ddee0fb0ddb4
CB <cryst1@yandex.ru>
2021-12-22 22:13:33 +0300

    up

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,7 +1,40 @@
-// const JwtStrategy = require('passport-jwt').Strategy
-// const ExtractJwt = require('passport-jwt').ExtractJwt
-// const keys 
-// const options  = {
-//     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//     secretOrKey: keys.jwt
-// }
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const keys = "test-jwt"
+
+const knex = require('knex')({
+    client: 'pg',
+    version: '13',
+    connection: {
+      host : '127.0.0.1',
+      port:'5432',
+      user : 'postgres',
+      password : 'admin',
+      database : 'Blog',
+      timezone: 'UTC',
+      dateStrings: true,
+    }
+});
+
+
+const options  = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys
+}
+
+module.exports = passport =>{
+    passport.use(
+        new JwtStrategy(options, async (payload, done) =>{
+           try{ 
+                const User = await knex.select('email','User_id').table('User').where('User',payload.User_id)
+                    if(User){
+                        done(null, User)
+                    }else{
+                        done(null, false)
+                }
+        } catch(e){
+            console.log(e)
+        }
+        })
+    )
+}

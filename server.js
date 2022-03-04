488f87442878589ced9df239fc79f1d9c6d22af5
CB <cryst1@yandex.ru>
2022-03-04 22:58:27 +0300

    minor up

--- server.js
+++ server.js
@@ -1,135 +1,23 @@
-const express = require('express');
-const app = express();
-const _ = require('underscore');
-const passport = require('passport');
+const express = require('express')
+const { use } = require('passport')
+const authRouter = require('./routes/auth')
+const blogRouter = require('./routes/blog')
 
-const upload =  require('./middleware/f_load')
-const jsonwt = require('jsonwebtoken');
-const key = "test-jwt";
-const { body, check, validationResult } = require('express-validator');
+const app = express()
+const passport = require('passport')
+const bodyparser =  require('body-parser')
 
-const fs = require('fs');
-
-const bcrypt = require('bcryptjs');
-
-
-const salt = bcrypt.genSaltSync(10)
-const knex = require('knex')({
-    client: 'pg',
-    version: '13',
-    connection: {
-      host : '127.0.0.1',
-      port:'5432',
-      user : 'postgres',
-      password : 'admin',
-      database : 'Blog',
-      timezone: 'UTC',
-      dateStrings: true,
-    }
-});
+app.use(bodyparser.json())
 
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
-app.use(express.json());
 
+app.use('/auth',authRouter)
+app.use('/blog',blogRouter)
 
+module.exports = app
 
-const validator =  function(req, res, next){
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        res.send(errors);
-        //console.log(errors);
-        fs.unlinkSync(req.file.path);
-    }
-    else{
-        next()
-    }
-};
-
-
-
-
-app.post('/registration', async (req,res) => {
-
-    //console.log(req.query.email != undefined)
-    if (req.query.email && req.query.User!= undefined){
-
-        const SameUsername = await knex.select('User').table('User').where('User',req.query.User)
-        const SameEmail = await knex.select('User').table('User').where('email',req.query.email)
-        
-
-        if(_.isEmpty(SameUsername) == false && _.isEmpty(SameEmail) == false){
-            res.status(409).json({message:'Имя пользователя и адресс почты заняты!'})
-        }
-  
-        else if( _.isEmpty(SameUsername) == false){
-            res.status(409).json({message: 'Имя пользователя занято. Пропробуйте другое'})
-        }
-    
-        else if(_.isEmpty(SameEmail) == false) {
-            res.status(409).json({message: 'Адрес электроной почты занят. Пропробуйте другой'})
-        }
-
-        else { 
-            const cryptpass = await bcrypt.hash(req.query.Password, salt)
-            await knex('User').insert({User:req.query.User, Password:cryptpass, email:req.query.email})
-            .then(res.status(201).json({message: 'ok'}))
-        }
-
-    }
-    else{
-        res.status(409).json({message: 'Не заполнены небходимые поля'})
-    }
-})
-
-app.post('/auth', async (req,res)=> {
-    
-    if (req.query.Password && req.query.User != undefined){
-        const ExistUsername = await knex.select('*').table('User').where('User',req.query.User)
-
-        if(_.isEmpty(ExistUsername) == false){
-            
-            const passwordResult = bcrypt.compareSync(req.query.Password, ExistUsername[0].Password)
-            if(passwordResult){
-                const token = jsonwt.sign({
-                    email: ExistUsername[0].email,
-                    User_id: ExistUsername[0].User_id
-
-                },key,{expiresIn: 3600})
-                res.status(200).json({token: `Bearer ${token}`})
-            }
-            else{
-                res.json({status:'not ok'})
-            }
-
-        }
-        
-    } else{
-        res.status(409).json({message: 'Не заполнены небходимые поля'})
-
-    }
-})
-
-
-
-
-app.post('/blog',upload.single('image'),body("Message").notEmpty(),validator, passport.authenticate('jwt', {session:false}),  async(req, res)=> {
-    await knex('BlogData').insert({User:req.body.User, Message:req.body.Message})
-    .then(res.status(201).json({message: 'ok'}))
-
-})
-
-app.get('/blog', passport.authenticate('jwt', {session:false}), async(req, res)=> {
-    console.log('get')
-    const result = await knex('BlogData').select('Message').where({User:req.body.User})
-    res.json(result)
-
-})
-
-
-
-
-const port = 9000
+const port = 9001
 app.listen(port,()=>{
     console.log(`server started at ${port}`)
 })
